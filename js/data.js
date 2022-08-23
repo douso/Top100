2b313acbb1ccd41e69cd6f62bc8d496b897c1130
difei <2223605608@qq.com>
2022-08-23 17:15:47 +0800

    t

--- js/data.js
+++ js/data.js
@@ -12,22 +12,26 @@ $(function() {
 })
 
 function getData(keyword) {
-    $('.bottom-box h4 a').text('');
+    $('.bottom-box h4 a').html('');
     $('.main ul').html('');
     $('.bottom-box .desc').text('');
     $('.bottom-box .pic img').attr('src', 'img/loading.svg');
     $.ajax({
         type: "GET",
-        url: `https://api.github.com/search/repositories?q=${keyword}+stars:>=10000&sort=stars&order=desc&per_page=100`,
+        url: `https://api.github.com/search/repositories?q=${keyword}+stars:>=5000&sort=stars&order=desc&per_page=100`,
         success: function(data) {
             render(data.items);
         }
     })
 }
 
+function formatStar(num) {
+    return num >= 1000 ? (num/1000).toLocaleString('zh-CN', {maximumFractionDigits: 1}) + 'k' : num;
+}
+
 function render(data) {
     const firstItem = data.shift();
-    $('.bottom-box h4 a').text(firstItem.name);
+    $('.bottom-box h4 a').html(`${firstItem.name}<span class="star"><img src="img/star.svg"/>${formatStar(firstItem.stargazers_count)}</span>`);
     $('.bottom-box h4 a').attr('href', firstItem.homepage);
     $('.bottom-box .pic img').attr('src', firstItem.owner.avatar_url);
     const description = filterHtmlTag(firstItem.description);
@@ -49,7 +53,7 @@ function render(data) {
             <span>${i+2}</span>
             <div class="pic"><img src="${data[i].owner.avatar_url}" alt=""></div>
             <div class="content">
-                <h4><a href="${hrefStr}" ${target}>${data[i].name}</a></h4>
+                <h4><a href="${hrefStr}" ${target}>${data[i].name}</a> <span class="star"><img src="img/star.svg"/>${formatStar(data[i].stargazers_count)}</span></h4>
                 <p>${description}</p>
             </div>
             <a href="${data[i].html_url}" target="_blank" class="github"><img src="img/github.svg" git/></a>
